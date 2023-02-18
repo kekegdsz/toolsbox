6bb2d326420093bb59350a554a839303b5f571f4
keke <keke@ky-tech.com.cn>
2023-02-18 15:18:50 +0800

    all code

---
+++ app/src/main/java/com/overarching/sky/box/BoxApp.java
@@ -0,0 +1,16 @@
+package com.overarching.sky.box;
+
+import android.app.Application;
+
+/**
+ * @author: KYE keke
+ * @email: keke@ky-tech.com.cn
+ * @date: 2023/2/18 14:51
+ */
+public class BoxApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
